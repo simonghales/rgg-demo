6bd8f16b89f0ac3eb790b4515653da47b3fed14c
Simon Hales <simonghales@gmail.com>
2021-02-04 21:45:29 +1000

    add default bamboo chunk

--- src/game/scenery/Scenery.tsx
+++ src/game/scenery/Scenery.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Editable } from "rgg-editor"
 import Floor from "./Floor"
 import Lights from "./Lights"
+import BambooChunk from "./BambooChunk";
 
 const Scenery: React.FC = () => {
     return (
@@ -10,6 +11,9 @@ const Scenery: React.FC = () => {
                 <Lights/>
             </Editable>
             <Floor/>
+            <Editable id="bambooChunk" width={10} height={5} y={10}>
+                <BambooChunk/>
+            </Editable>
         </>
     )
 }
