291f6753bdd1966ff9e3c4496704f81c2e7daed0
Simon Hales <simonghales@gmail.com>
2021-02-09 18:46:30 +1000

    add manual display name

--- src/3d/assets.tsx
+++ src/3d/assets.tsx
@@ -5,10 +5,16 @@ export const FloorTile: React.FC = () => {
     return <Asset path="/models/spaceship/FloorTile_Basic.glb" position={[0, 0, -0.1]}/>
 }
 
+FloorTile.displayName = 'Floor Tile'
+
 export const Wall: React.FC = () => {
     return <Asset path="/models/spaceship/Wall_1.glb" position={[0, 0, -0.1]}/>
 }
 
+Wall.displayName = 'Wall'
+
 export const Column: React.FC = () => {
     return <Asset path="/models/spaceship/Column_1.glb" position={[0, 0, -0.1]}/>
 }
+
+Column.displayName = 'Column'
