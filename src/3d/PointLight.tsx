c227f95fccdace34c0c5bce7a942b543cdc4ff52
Simon Hales <simonghales@gmail.com>
2021-02-14 22:31:26 +1000

    tweak light bias

--- src/3d/PointLight.tsx
+++ src/3d/PointLight.tsx
@@ -4,7 +4,7 @@ import {SelectableGroup} from "../temp";
 const PointLight: React.FC = () => {
     return (
         <SelectableGroup>
-            <pointLight intensity={0.25} castShadow shadowBias={-0.0001} />
+            <pointLight intensity={0.25} castShadow shadowBias={-0.001} />
         </SelectableGroup>
     )
 }
